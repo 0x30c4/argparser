cc0d2609581d036242bb72e055bed388a9fda7d1
0x30c4 <abdullahsanaf@gmail.com>
2019-12-17 21:53:13 +0600

    Starting again :(

--- ArgPars.java
+++ ArgPars.java
@@ -1,166 +1,208 @@
 import java.util.*;
 
 public class ArgPars{
-	private List<String> positional_args = new ArrayList<>();
-	private List<String> optional_args = new ArrayList<>();
-	private List<String> opt_pos_long = new ArrayList<>();
-	private List<String> opt_pos_short_chr = new ArrayList<>();
-	private List<String> opt_pos_long_chr = new ArrayList<>();
 	private List<String> opt_pos_short = new ArrayList<>();
-	private List<String> opt_pos_name = new ArrayList<>();
-	private List<String> arg_cmp = new ArrayList<>();
-	private List<String> _args = new ArrayList<String>();
+	private List<String> opt_pos_short_char = new ArrayList<>();
+	private List<String> opt_pos_long = new ArrayList<>();
+	private List<String> opt_pos_long_name = new ArrayList<>();
+	
+	private List<String> no_val_short = new ArrayList<>();
+	private List<String> no_val_short_char = new ArrayList<>();
+	
+	private List<String> positional_arg = new ArrayList<>();
+	private List<String> args = new ArrayList<String>();
+	
+	public String[] supportedDataTypes = {"str", "int", "float", "double", "boolean"};
 
-	Hashtable<String, Boolean> positional_arg = new Hashtable<String, Boolean>(); 
-	Hashtable<String, String> positional_arg_value = new Hashtable<String, String>(); 
+	public Hashtable<String, Boolean> optional_arg = new Hashtable<String, Boolean>(); 
+	public Hashtable<String, String> optional_arg_value = new Hashtable<String, String>(); 
 
 	public ArgPars(String[] aa){
 		for (String i: aa) {
-			this._args.add(i);
+			this.args.add(i);
 		}
-		// for (int i = 0 ; i <= this._args.size(); i++) {
-		// 	System.out.println(this._args.get(i));
-		// }
-	}
-	public void AddOpt(String[] opt_detail){
-	/*		
-		If there is no use of any if the element then put "NONE" to maintain the
-		correct order.
-		Element No:
-		  0 | If the option dose not consist any argument then use short option. [ -f ]
-		  1 | Long option or the double dash option if there is any argument. [ --file-name ]
-		  2 | Use it if there is any argument for the long option. [ --file-name=foo.txt || --file-name foo.txt ]
-		  3 | This is help string for the option
-	*/
-		// Error check.
-		boolean e = false;
-		if (opt_detail.length != 4){
-			System.out.println("Error: Need exactly four items.");
-		}else{
-			for (int i = 0; i != 4; i++) {
-				if ((((opt_detail[i].startsWith("-") || 
-						opt_detail[i].charAt(1) == '-') || i >= 2)
-						|| opt_detail[i].equals("NONE") )) {
-					if (i <= 1){
-						this.arg_cmp.add(opt_detail[i]);
-					}
-				}else{
-					e = true;
-				}
-				if (!opt_detail[1].equals("NONE") && opt_detail[2].equals("NONE")) {
-					e = true;
-				}
-			}
-			if (e){
-				System.err.println("Error: some thing is missing.");
-				System.exit(0);
-			}
-		}
-		/* 
-		Finds the optional arguments (short option) and adding them to the "optional_args" list.
-		*/
-		if (opt_detail[1].equals("NONE") && opt_detail[2].equals("NONE")){
-			this.optional_args.add(opt_detail[0]);
-		}
-		/* 
-		Finds the positional arguments and adding them to the "positional_args" list.
-		*/
-		if (opt_detail[0].equals("NONE") && opt_detail[1].equals("NONE")){
-			this.positional_args.add(opt_detail[2]);
-		}
-		/* 
-		Finds the optional arguments (long option) with positional value and adding them to the "optional_positional_args" list.
-		*/
-		if (!opt_detail[0].equals("NONE") && !opt_detail[1].equals("NONE")
-		 	&& !opt_detail[2].equals("NONE")){
-			this.opt_pos_short.add(opt_detail[0]);
-			this.opt_pos_long.add(opt_detail[1]);
-			this.opt_pos_name.add(opt_detail[2]);
-		}		
 	}
 
+	/** 
+	*For defining a positional argument just fill other
+	*parameters with "NONE".
+	*@param short_opt  If the option dose not consist any
+	*				   argument then just use short option.
+	*@param	long_opt   Long option or the double dash 
+	*				   option if there is any argument.
+	*@param value_type Use it if there is any argument 
+	*			       for the long option.
+	*@param value      Define the data type that need to 
+	*				   be parsed as positional or optional
+	*				   argument.There is five types of data
+	*				   is supported [str, int, float, double, boolean].
+	*@param help_str   This is help string for the option.
+	*/
+	public void AddOpt(String short_opt, 
+						String long_opt, String value, 
+						String value_type, String help_str){
+
+		// Error check.
+		if (!short_opt.startsWith("-") && short_opt.length() != 2 && 
+				!this.checkIfNone(value) && !this.checkIfNone(value_type) &&
+				!this.checkIfNone(long_opt)) {
+			System.out.println(
+				"Error [Short option]: A short option should allays consist of two char.A '-' followed by a single character\n"
+				);
+			System.out.println(short_opt + ", " + long_opt + ", " + value + ", " + value_type + ", " + help_str);
+			System.exit(0);
+		}else if (this.checkIfNone(help_str)){
+			System.out.println("Error [Help]: Help menu can't be NONE.");
+			System.out.println(short_opt + ", " + long_opt + ", " + value + ", " + value_type + ", " + help_str);		
+			System.exit(0);
+		}else if ((!this.checkIfLongArg(long_opt)) && !this.checkIfNone(long_opt) || long_opt.length() < 3) {
+			System.out.println("Error [Long option]: Long option should consist two '-' or dash and should be more then a 1 word.");
+			System.out.println(short_opt + ", " + long_opt + ", " + value + ", " + value_type + ", " + help_str);
+			System.exit(0);
+		}else if (this.checkIfNone(value_type) &&
+					!this.checkIfNone(long_opt) && !this.checkIfNone(value)){
+			System.out.println("Error [Data type]: Need to specify the data type if your using a argument");
+			System.out.println(short_opt + ", " + long_opt + ", " + value + ", " + value_type + ", " + help_str);
+			System.exit(0);
+		}else if (!this.dataType(value_type) && !(this.checkIfNone(long_opt) && this.checkIfNone(value) &&
+					this.checkIfNone(value_type))) {
+			System.out.println("Error [Data type]: This data type is not supported.But this are : [str, int, float, double, boolean]");
+			System.out.println(short_opt + ", " + long_opt + ", " + value + ", " + value_type + ", " + help_str);
+			System.exit(0);
+		}else if (this.checkIfNone(short_opt) && !this.checkIfNone(long_opt)){
+			
+		}
+
+		// Checking for duplicate
+		if (this.checkIfMatch(short_opt, opt_pos_short) || this.checkIfMatch(long_opt, this.opt_pos_long) ||
+			this.checkIfMatch(value, positional_arg) || this.checkIfMatch(short_opt, no_val_short)){
+			System.out.println("Error [Duplicate found]: Can't use same things twice.");
+			System.out.println(short_opt + ", " + long_opt + ", " + value + ", " + value_type + ", " + help_str);
+			System.exit(0);
+		}
+
+		/* 
+		*Finds the optional options (short option).
+		*/
+		if (!this.checkIfNone(short_opt) && this.checkIfNone(long_opt)){
+			this.no_val_short.add(short_opt);
+			this.no_val_short_char.add(short_opt.substring(1));
+		}
+
+		/* 
+		*Finds the optional long options.
+		*/
+		if (!this.checkIfNone(long_opt) && !this.checkIfNone(short_opt)){
+			this.opt_pos_short.add(short_opt);
+			this.opt_pos_short_char.add(short_opt.substring(1));
+			this.opt_pos_long.add(long_opt);
+			this.opt_pos_long_name.add(value);
+		}
+
+		/* 
+		*Finds the positional argument.
+		*/
+		if (!this.checkIfNone(value) && this.checkIfNone(long_opt) &&
+			this.checkIfNone(short_opt)){
+			this.positional_arg.add(value);
+		}
+	}
+
+	/**@param arg will be checked against the list given as parameter for any match case.
+	*@param list which will be checked.
+	*@return True if the any match case.False if it's not.
+	*/
+	public boolean checkIfMatch(String arg, List<String> list){
+		int q = 0;
+		for (String i: list) {
+			if (arg.equals(i))
+				q = 1;
+		}
+		return q == 1 ? true : false;
+	}
+
+	/**@param arg It will be checked if it's a supported data type.
+	*@return True if the given parameter is a supported data type.False if it's not.
+	*/
+	public boolean dataType(String arg){
+		int q = 0;
+		for (String i: this.supportedDataTypes) {
+			if (i.equals(arg))		
+				q = 1;
+		}
+		return q == 1 ? true : false;
+	}
+
+	/**@param arg It will be checked if it's equal to "NONE".
+	*@return True if the given parameter is equal to "NONE".False if it's not.
+	*/
+	public boolean checkIfNone(String arg){
+		if (arg.equals("NONE")){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	/**@param arg It will be checked if it's a long option or not.
+	*@return True if the given option is a long option.False if it's not.
+	*/
+	public boolean checkIfLongArg(String arg){
+		if (arg.startsWith("-") && arg.charAt(1) == '-'){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	//This method parse the argument's that user gave
 	public void parse_arg(){
-		String positional_value = "null";
+		String arg;
 		boolean invalid_arg_err_sort = false;
 		boolean invalid_arg_err_long = false;
 		boolean need_arg_err_value = false;
-		int index;
-		// Parsing optional arguments.
-		for (int i = 0 ; i <= this._args.size() - 1 ; i++) {
-			// Looking for the short options.
-			if (this._args.get(i).startsWith("-") && this._args.get(i).charAt(1) != '-') {
-				for (String _so: this.optional_args) {
-					index = this.optional_args.indexOf(_so);
-					if (_so.equals(this._args.get(i))){
-							this.positional_arg.put(this.optional_args.get(index), true);
-					}else {
-						invalid_arg_err_sort = true;
-						// System.out.println(this._args.get(i));
-					}
-				}
-				// Looking for optional values provided after the short option.
-				for (String _sa: this.opt_pos_short) {
-					index = this.opt_pos_short.indexOf(_sa);
-					try {	
-						if (this._args.get(i).equals(_sa) && 
-							(this._args.size() > i + 1)){
-							positional_value = this._args.get(i + 1);
-							this.positional_arg_value.put(this.opt_pos_name.get(index), positional_value);
-							if (invalid_arg_err_sort)
-								invalid_arg_err_sort = false;
+		int ii;
+
+		for ( int i = 0; i != this.args.size() ; i++ ) {
+			arg = this.args.get(i);
+			try{
+			// Finding all the short optional options whether they consist any positional argument or not.
+				if (arg.startsWith("-") && arg.charAt(1) != '-' && arg.length() == 2){
+					if (this.checkIfMatch(arg, this.no_val_short)){
+						ii = this.no_val_short.indexOf(arg);
+						this.optional_arg.put(this.no_val_short_char.get(ii), true);
+					}else if (this.checkIfMatch(arg, this.opt_pos_short)) {
+						if (this.args.size() > i + 1 && !this.args.get(i + 1).startsWith("-")) {
+							ii = this.opt_pos_short.indexOf(arg);
+							this.optional_arg_value.put(this.opt_pos_long_name.get(ii), this.args.get(i + 1));
 						}else {
 							need_arg_err_value = true;
 						}
-						if (this._args.get(i).substring(0, 2).equals(_sa)) {
-							positional_value = this._args.get(i).substring(2, this._args.get(i).length());
-							this.positional_arg_value.put(opt_pos_name.get(index), positional_value);
-							if (invalid_arg_err_sort)
-								invalid_arg_err_sort = false;							
-						}
-						if (!this._args.get(i).equals(_sa) &&
-							!this._args.get(i).substring(0, 2).equals(_sa)) {
-							invalid_arg_err_sort = true;
-							System.out.println(this._args.get(i));
-							System.out.println(this._args.get(i).equals(_sa) + _sa);
-						}
-					}catch (Exception e){
-						// System.out.println(e);
+						// System.out.println(this.args.size() > i + 1 && !this.args.get(i + 1).startsWith("-"));
 					}
-			
-			// Error detection 			
-			if (invalid_arg_err_sort || invalid_arg_err_long) {
-				System.out.println("Invalid argument error 11");
-				System.out.println(invalid_arg_err_sort +" "+ invalid_arg_err_long);
-				System.exit(0);
-			}else if (need_arg_err_value) {
-				System.out.println("Need an argument");
-				System.exit(0);
-			}					
+				}else if (arg.startsWith("-") && arg.charAt(1) != '-') {
+					// this._args.get(i).substring(0, 2);
+					System.out.println(arg);
 
 				}
-				// Some change needed in the near future.
-			}else if (this._args.get(i).startsWith("-") && this._args.get(i).charAt(1) == '-') {// && this._args[i].length() => 3) {
-			// Looking for long options.
-				for (int ls = 0 ; ls <= this.opt_pos_long.size() ; ls++) {
-					try {
-						if (this._args.get(i).equals(this.opt_pos_long.get(ls))){
-							if (!(this._args.size() < (i + 2))){
-								positional_value = this._args.get(i + 1);
-								this.positional_arg_value.put(this.opt_pos_name.get(ls), positional_value);
-							}else {
-							// Need an argument.Not done yet. :v
-								System.out.println("Need an argument");
-							}
-						}else if (this._args.get(i).charAt(this.opt_pos_long.get(ls).length()) == '=' &&
-								 	this._args.get(i).substring(0, this.opt_pos_long.get(ls).length()).equals(this.opt_pos_long.get(ls))){
-							positional_value = this._args.get(i).substring(this.opt_pos_long.get(ls).length() + 1, this._args.get(i).length());
-							this.positional_arg_value.put(this.opt_pos_name.get(ls), positional_value);
-						}
-					}catch (Exception e){
-						// System.out.println(e);
-					}
-				}
+			}catch (Exception e){
+				// System.out.println(e);
 			}
+
+				// else if (this.args.get(i).length() > 2) {
+				// 	for (char aa: this.args.get(i).toCharArray()) { //this.args.get(i).toCharArray()
+				// 		// if (this.args.get(i).indexOf(aa) != -1) {
+				// 		// 	ii = this.no_val_short_char.indexOf(aa);
+				// 		// 	this.optional_arg.put(this.no_val_short_char.get(ii), true);
+				// 		// 	// System.out.println(this.args.get(i).indexOf(aa));				
+				// 		// }else{
+				// 		// 	invalid_arg_err_sort = true;
+				// 		// }
+				// 			System.out.println(aa);
+				// 	}
+				// }else {
+				// 	invalid_arg_err_sort = true;
+				// }
 		}
 	}
 
