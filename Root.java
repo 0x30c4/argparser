0ae9644c93d03965af7f4c6fa7f3a79ea855d34d
0x30c4 <abdullahsanaf@gmail.com>
2019-12-24 17:54:53 +0600

    This is just for testing the 'ArgParse' class

--- Root.java
+++ Root.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class Root{
 	public static void main(String[] args) {
+		
+		// creating an object.
+		// the 1st parameter is the "args" that user supply's.
+		// 2nd is the program name.
+		// 3rd is the program description.
 		ArgPars a = new ArgPars(args, "test", "testing");
 		
 		// Adding the cli options.
