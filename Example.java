14f1200386b35262c52d9fdd628bb8c42da4923a
0x30c4 <abdullahsanaf@gmail.com>
2019-12-28 22:42:18 +0600

    This is just an example program for demonstrating the usage of 'ArgParse' class.

--- Example.java
+++ Example.java
@@ -24,22 +24,22 @@ public class Example{
 
 		// Showing the result.
 		
-		// If there is any defined optional options then the "optional_arg" Hashtable will 
+		// If there is any defined optional options then the "optionalArg" Hashtable will 
 		// store a "true" value for that option like the example.Else that will store a "null".
 		// Now it's up to you how would you use it.
-		if (a.optional_arg.get("q") != null)
+		if (a.optionalArg.get("q") != null)
 			System.out.println("The [q] option is used.");
 		
-		// If there is any defined optional long option then the "optional_arg_value" Hashtable will 
+		// If there is any defined optional long option then the "optionalArgValue" Hashtable will 
 		// store the argument value for that option like the example.Else that will store a "null".
-		if (a.optional_arg_value.get("ip_addr") != null)
-			System.out.println("The ip is : " + a.optional_arg_value.get("ip_addr"));
+		if (a.optionalArgValue.get("ip_addr") != null)
+			System.out.println("The ip is : " + a.optionalArgValue.get("ip_addr"));
 		
-		if (a.optional_arg_value.get("port") != null)
-			System.out.println("The port number is : " + a.optional_arg_value.get("port"));
+		if (a.optionalArgValue.get("port") != null)
+			System.out.println("The port number is : " + a.optionalArgValue.get("port"));
 
 		// This are the positional argument.When the program will run user must put those.
-		if (a.positional_argument.get("file") != null)
-			System.out.println("The file name is : " + a.positional_argument.get("file"));
+		if (a.positionalArgument.get("file") != null)
+			System.out.println("The file name is : " + a.positionalArgument.get("file"));
 	}
 }
